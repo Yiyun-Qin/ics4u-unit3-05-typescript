7659a4b5350f0691f45ed6919ffb1b46745a3a71
Yiyun Qin <99287007+Yiyun-Qin@users.noreply.github.com>
2023-01-24 03:55:56 +0000

    magic square

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@
  * This program prints out
  * the Magic Squares.
  *
- * @author  Nicholas B. & Mr. Coxall
- * @version 1.0
- * @since   2020-01-01
+ * Author:  Nicholas B. & Mr. Coxall
+ * Version: 1.0
+ * Since:   2020-01-01
  */
 
 /** The middle left index. */
@@ -24,14 +24,22 @@ const NINE: number = 9
 /** The maximum number for magicNumbers. */
 const MAGICNUM: number = 15
 
-function genSquare(
+/**
+ * Generate a magic square.
+ *
+ * @param {number[]} square The square
+ * @param {number[]} currentSquare Current magic square
+ * @param {number[]} used If the number is used
+ * @param {number} index The index of the magic square
+ */
+function genSquare (
   square: number[],
   currentSquare: number[],
   used: number[],
   index: number
 ): void {
   for (let counter = 0; counter < currentSquare.length; counter++) {
-    if (used[counter] == 0) {
+    if (used[counter] === 0) {
       currentSquare[index] = square[counter]
       used[counter] = 1
       if (index < currentSquare.length) {
@@ -45,47 +53,59 @@ function genSquare(
   }
 }
 
-function isMagic(preSquare: number[]): boolean {
+/**
+ * Check if it is a magic square.
+ *
+ * @param {number[]} preSquare The magic square
+ * @returns {boolean} is a square or not
+ */
+function isMagic (preSquare: number[]): boolean {
   // returns true or false for whether or not array is a magic square
-  let row1: number = preSquare[0] + preSquare[1] + preSquare[2]
-  let row2: number = preSquare[THREE] + preSquare[FOUR] + preSquare[FIVE]
-  let row3: number = preSquare[SIX] + preSquare[SEVEN] + preSquare[EIGHT]
-  let col1: number = preSquare[0] + preSquare[THREE] + preSquare[SIX]
-  let col2: number = preSquare[1] + preSquare[FOUR] + preSquare[SEVEN]
-  let col3: number = preSquare[2] + preSquare[FIVE] + preSquare[EIGHT]
-  let diag1: number = preSquare[0] + preSquare[FOUR] + preSquare[EIGHT]
-  let diag2: number = preSquare[2] + preSquare[FOUR] + preSquare[SIX]
+  const row1: number = preSquare[0] + preSquare[1] + preSquare[2]
+  const row2: number = preSquare[THREE] + preSquare[FOUR] + preSquare[FIVE]
+  const row3: number = preSquare[SIX] + preSquare[SEVEN] + preSquare[EIGHT]
+  const col1: number = preSquare[0] + preSquare[THREE] + preSquare[SIX]
+  const col2: number = preSquare[1] + preSquare[FOUR] + preSquare[SEVEN]
+  const col3: number = preSquare[2] + preSquare[FIVE] + preSquare[EIGHT]
+  const diag1: number = preSquare[0] + preSquare[FOUR] + preSquare[EIGHT]
+  const diag2: number = preSquare[2] + preSquare[FOUR] + preSquare[SIX]
 
   return (
-    row1 == MAGICNUM &&
-    row2 == MAGICNUM &&
-    row3 == MAGICNUM &&
-    col1 == MAGICNUM &&
-    col2 == MAGICNUM &&
-    col3 == MAGICNUM &&
-    diag1 == MAGICNUM &&
-    diag2 == MAGICNUM
+    row1 === MAGICNUM &&
+    row2 === MAGICNUM &&
+    row3 === MAGICNUM &&
+    col1 === MAGICNUM &&
+    col2 === MAGICNUM &&
+    col3 === MAGICNUM &&
+    diag1 === MAGICNUM &&
+    diag2 === MAGICNUM
   )
 }
 
-function printMagicSquare(outputSquare: number[]): void {
+/**
+ * Print the magic square.
+ *
+ * @param {number[]} outputSquare The print
+ */
+function printMagicSquare (outputSquare: number[]): void {
   // prints inputted array in a magic square format
   console.log('\n*****')
+  let result = ''
   for (let count = 0; count < outputSquare.length; count++) {
-    if (count == THREE || count == SIX) {
-      console.log()
-      console.log(outputSquare[count] + ' ')
+    if (count === 2 || count === FIVE) {
+      result += outputSquare[count].toString() + '\n'
     } else {
-      console.log(outputSquare[count] + ' ')
+      result += outputSquare[count].toString() + ' '
     }
   }
+  console.log(result)
   console.log('\n*****')
 }
 
 // main stub, get user input here
-let magicSquare = new Array(1, 2, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE)
-let extraArray = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0)
-let used = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0)
+const magicSquare = [1, 2, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE]
+const extraArray = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+const used = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 console.log('\n')
 console.log('All Possible Magic Squares (3x3):\n')
 genSquare(magicSquare, extraArray, used, 0)
